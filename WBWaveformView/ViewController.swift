47e89695261cb21211bbf34f2f34d43e6ebe3899
WesleyB <wesleyj101@gmail.com>
2017-07-05 14:04:44 -0700

    Initial Commit

---
+++ WBWaveformView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WBWaveformView
+//
+//  Created by Wesley Bevins on 7/5/17.
+//  Copyright © 2017 Wesley Bevins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
